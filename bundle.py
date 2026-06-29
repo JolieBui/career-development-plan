be22868ddda8aba3d30e8be382b6eba5e02fd24e
JolieBui <nhibui281102@gmail.com>
2026-06-29 22:18:32 +0700

    Bump cache version to force responsive CSS update

--- bundle.py
+++ bundle.py
@@ -13,12 +13,12 @@ with open(os.path.join(project_dir, "app.js"), "r", encoding="utf-8") as f:
     js_content = f.read()
 
 # Replace CSS link
-css_link = '<link rel="stylesheet" href="styles.css?v=2.4">'
+css_link = '<link rel="stylesheet" href="styles.css?v=2.8">'
 embedded_css = f"<style>\n{css_content}\n</style>"
 html_content = html_content.replace(css_link, embedded_css)
 
 # Replace JS script tag
-js_script = '<script src="app.js?v=2.7"></script>'
+js_script = '<script src="app.js?v=2.8"></script>'
 embedded_js = f"<script>\n{js_content}\n</script>"
 html_content = html_content.replace(js_script, embedded_js)
 
